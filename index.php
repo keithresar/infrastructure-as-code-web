d4d4923a24853c4f283677c369fe138d09856aa1
Your Name <you@example.com>
2019-02-20 23:04:14 +0000

    -

--- index.php
+++ index.php
@@ -1,5 +1,40 @@
-<?php 
 
-phpinfo();
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Language Wizard</title>
 
-?>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Language Wizard</a>
+    </nav>
+
+    <main role="main" class="container" style="margin-top:6em;">
+
+      <form class="form-inline">
+        <div class="form-group">
+          <label for="url">Give URL to Scan</label>
+          <input style="margin-left:2em; margin-right:2em;" type="text" class="form-control" id="url" placeholder="http://example.com">
+        </div>
+        <button type="button" id="submit_url_btn" class="btn btn-primary">Submit</button>
+      </form>
+      <hr>
+
+    </main><!--container -->
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+
+  <script type="text/javascript">
+  $(document).ready(function() {
+
+  });
+  </script>
+
+  </body>
+</html>
