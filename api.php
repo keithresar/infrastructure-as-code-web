b045366f6650018589e509f23fd0498eeb28484d
Keith Resar <keithresar@users.noreply.github.com>
2019-03-04 16:57:37 -0600

    Update api.php

--- api.php
+++ api.php
@@ -2,12 +2,10 @@
 
 require_once("configure.php");
 
-global $API_SERVER_IP, $API_SERVER_PORT;
-
 if (!array_key_exists('url',$_REQUEST))  exit;
 
 
-print json_encode(json_decode(file_get_contents(sprintf("http://%s:%s/?url=%s",$API_SERVER_IP,$API_SERVER_PORT,urlencode($_REQUEST['url'])))),JSON_PRETTY_PRINT);
+print json_encode(json_decode(file_get_contents(sprintf("http://%s:%s/?url=%s",API_SERVER_IP,API_SERVER_PORT,urlencode($_REQUEST['url'])))),JSON_PRETTY_PRINT);
 
 
 
